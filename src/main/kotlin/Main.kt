df3891d0e51069cf4aaa0e73721a76d30b6cb337
Alexis Ortega <alexis.oc3@gmail.com>
2024-08-04 17:26:17 -0600

    Create simple pipeline

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,16 +1,42 @@
 package org.alexiscrack3
 
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-fun main() {
-    val name = "Kotlin"
-    //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-    // to see how IntelliJ IDEA suggests fixing it.
-    println("Hello, " + name + "!")
+import org.apache.beam.runners.direct.DirectRunner
+import org.apache.beam.sdk.Pipeline
+import org.apache.beam.sdk.coders.StringUtf8Coder
+import org.apache.beam.sdk.io.TextIO
+import org.apache.beam.sdk.options.PipelineOptionsFactory
+import org.apache.beam.sdk.transforms.Create
+import org.apache.beam.sdk.transforms.DoFn
+import org.apache.beam.sdk.transforms.ParDo
+import org.apache.beam.sdk.values.PCollection
 
-    for (i in 1..5) {
-        //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-        // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-        println("i = $i")
+
+val LINES: List<String> = mutableListOf(
+    "To be, or not to be: that is the question: ",
+    "Whether 'tis nobler in the mind to suffer ",
+    "The slings and arrows of outrageous fortune, ",
+    "Or to take arms against a sea of troubles, "
+)
+
+class LineFormatter: DoFn<String, String>() {
+    @ProcessElement
+    fun processElement(@Element line: String, out: OutputReceiver<String?>) {
+        out.output(line.uppercase())
     }
+}
+
+
+fun main() {
+    val options = PipelineOptionsFactory.create()
+    options.runner = DirectRunner::class.java
+    val p = Pipeline.create(options)
+
+    val input: PCollection<String> = p.apply(Create.of(LINES)).setCoder(StringUtf8Coder.of())
+
+    val transformation: ParDo.SingleOutput<String, String> = ParDo.of(LineFormatter())
+    val output = input.apply(transformation)
+    output.apply("WriteToText", TextIO.write().to("myfile").withSuffix(".txt"));
+
+
+    p.run()
 }
